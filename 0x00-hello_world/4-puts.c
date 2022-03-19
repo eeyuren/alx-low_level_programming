01b9257b0bd1455913754c06f49879b92f77a47c
eeyuren <eeyuren1@gmail.com>
2022-03-19 15:55:20 -0700

    Code to print the phrase using the function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/** To print the line Programming is like building a multilingual puzzle
+ * Using the function puts
+ * Return 0: return the entry point int as 0
+ */
+
+int main(void)
+	{
+		puts("rogramming is like building a multilingual puzzle\n");
+		return (0);
+	}
