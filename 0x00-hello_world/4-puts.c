d5527d4c9ecc370352e75c0875d320315c8fa201
eeyuren <eeyuren1@gmail.com>
2022-03-19 16:09:25 -0700

    print out the phrase using the function puts ad adding a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-/** To print the line Programming is like building a multilingual puzzle
+/**
+ * To print the line Programming is like building a multilingual puzzle
  * Using the function puts
  * Return 0: return the entry point int as 0
  */
-
 int main(void)
-        {
-                puts("\"Programming is like building a multilingual puzzle");
+	{
+         	puts("\"Programming is like building a multilingual puzzle");
                 return (0);
         }
-
