14ec5a11383c736524f81a66535389ebcd9939b6
eeyuren <eeyuren1@gmail.com>
2022-06-09 06:58:52 -0700

    A function that multiplies two values x and y

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Multiply two values
+ * @x: First value
+ * @y: Second value
+ *
+ * Return: result of x*y.
+ */
+int mul(int x, int y)
+{
+	int z = x * y;
+
+	return (z);
+}
